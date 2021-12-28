6d8b1f6b192298cdbd1cb23c06338b46c81800eb
unknown <faizaanshaikh2002>
2021-12-28 11:07:02 +0530

    15 videos completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import './App.css';
+// import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 function App() {
+  const [modeState, setModeState] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (modeState === "light") {
+      setModeState('dark')
+      document.body.style.backgroundColor = 'rgb(37, 40, 44)'
+      document.body.style.color = 'white'
+      showAlert("Dark Mode has been enabled", "success")
+    } else {
+      setModeState('light')
+      document.body.style.backgroundColor = 'white'
+      document.body.style.color = 'black'
+      showAlert("Light Mode has been enabled", "success")
+    }
+  }
   return (
-    // <div className="blank">Lovely</div>
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={modeState} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text below to analyze" />
+        <TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={modeState} />
         {/* <About /> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,35 +7,50 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toLocaleUpperCase();
         setText(newText)
+        props.showAlert("Converted to UpperCase", "success")
     }
+
     const handleLowClick = () => {
         let newText = text.toLocaleLowerCase();
         setText(newText)
+        props.showAlert("Converted to LowerCase", "success")
     }
+
     const handleClearClick = () => {
         let newText = "";
         setText(newText)
+        props.showAlert("Successfully cleared content", "success")
     }
+
+    const handleCopy = () => {
+        let text = document.getElementById("myBox")
+        text.select();
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Copied to Clipboard!", "success")
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
+
     return (
         <>
             <div className='container'>
                 <div className="mb-3">
                     <h1>{props.heading}</h1>
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'rgb(37, 40, 44)' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert To LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Content</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert To LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Content</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Content</button>
             </div>
             <div className="container my-2">
                 <h2>Your text summary</h2>
-                <p><b>{text.split(" ").length - 1} words and {text.length} characters</b></p>
+                <p><b>{(text.split(" ").length - 1) === 0 ? text.split(" ").length - 1 : text.split(" ").length} words and {text.length} characters</b></p>
                 <p><b>{Math.round(0.008 * text.split(" ").length)} Minutes to read</b></p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter Something to preview here"}</p>
             </div>
         </>
     )
