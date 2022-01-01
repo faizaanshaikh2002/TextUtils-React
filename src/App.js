d4bbf333dde2f508c736441f82301367e27219f6
Faizaan shaikh <sfaizan974@gmail.com>
2022-01-01 21:24:01 +0530

    Removed Router to deploy on github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-import {
-  Route,
-  Routes,
-} from "react-router-dom";
+// import {
+// Route,
+// Routes,
+// } from "react-router-dom";
 
 function App() {
   const [modeState, setModeState] = useState('light')
@@ -41,10 +41,11 @@ function App() {
       <Navbar title="TextUtils" mode={modeState} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className='container my-3'>
-        <Routes>
+        {/* <Routes>
           <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={modeState} />} />
           <Route exact path="/about" element={<About mode={modeState} />} />
-        </Routes>
+        </Routes> */}
+        <TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={modeState} />
       </div>
     </>
   );
